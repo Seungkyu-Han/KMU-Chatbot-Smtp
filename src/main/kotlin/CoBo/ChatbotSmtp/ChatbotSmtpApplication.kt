bfa6238851bef8bb378b0e4523941b52299256ed
trust1204@gmail.com <trust1204@gmail.com>
2024-01-04 14:24:25 +0900

    first commit

---
+++ src/main/kotlin/CoBo/ChatbotSmtp/ChatbotSmtpApplication.kt
@@ -0,0 +1,11 @@
+package CoBo.ChatbotSmtp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ChatbotSmtpApplication
+
+fun main(args: Array<String>) {
+	runApplication<ChatbotSmtpApplication>(*args)
+}
---
+++ src/test/kotlin/CoBo/ChatbotSmtp/ChatbotSmtpApplicationTests.kt
@@ -0,0 +1,13 @@
+package CoBo.ChatbotSmtp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ChatbotSmtpApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
